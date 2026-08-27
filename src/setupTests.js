eae1cc1dc91fe995ae9e1b75935e9d2d22cdef87
Yug Shah <yugshah197@gmail.com>
2026-08-27 20:04:45 +0530

    ai-upgrade: improve setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,14 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+/**
+ * Test environment setup entry point.
+ *
+ * Imports `@testing-library/jest-dom` to register custom Jest matchers for
+ * asserting on DOM nodes. This enables expressive assertions such as:
+ *
+ *   expect(element).toHaveTextContent(/react/i);
+ *   expect(element).toBeInTheDocument();
+ *   expect(element).toBeVisible();
+ *
+ * @see {@link https://github.com/testing-library/jest-dom|jest-dom documentation}
+ */
+
 import '@testing-library/jest-dom';
